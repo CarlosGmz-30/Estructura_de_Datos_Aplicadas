d3e203cd49131667b55cac7bc9ce60fa3cf5d5df
Martínez_Carlos <20223tn066@utez.edu.mx>
2023-10-03 12:26:57 -0600

    Busqueda

---
+++ BusquedaSecuencial/Main.java
@@ -0,0 +1,24 @@
+package BusquedaSecuencial;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 7, 9, 1, 2, 8 };
+        int busqueda = 4;
+        int indice = busqueda(arr, busqueda);
+        if(indice>=0){
+            System.out.println("El avlor " + busqueda + " se encuentra en el indice " + indice);
+        } else {
+            System.out.println("El valor " + busqueda + " no se encuentra en el arreglo");
+        }
+    }
+
+    public static int busqueda(int[] arr, int busqueda) {
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == busqueda){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
