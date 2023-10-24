415cd5e3c0782e1a616c41ead469b2689c9f77f2
Carlos <20223tn066@utez.edu.mx>
2023-10-24 13:01:31 -0600

    Camios

---
+++ Practicas/listasSimples/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Listas l = new Listas();
+
+        l.insertarNodo();
+        l.insertarNodo();
+        l.insertarNodo();
+        l.insertarNodo();
+        l.insertarNodo();
+
+        System.out.println("\nLos nodos ingresados son:");
+        l.desplegarLista();
+
+
+    }
+}
--- Temas/Lista/actividadesListas/becarios/Main.java
+++ Temas/Lista/actividadesListas/becarios/Main.java
@@ -13,16 +13,16 @@ Después presenta cada lista. Los datos pueden ser estáticos.
 public class Main {
     public static void main(String[] args) {
         // Zona de incialización de alumnos
-        Estudiante e1 = new Estudiante("Carlos", 4, 9.8);
+        Estudiante e1 = new Estudiante("Carlos", 4, 10);
         Estudiante e2 = new Estudiante("Keyla", 4, 9.4);
-        Estudiante e3 = new Estudiante("Daniela", 3, 8.7);
+        Estudiante e3 = new Estudiante("Daniela", 3, 9.7);
         Estudiante e4 = new Estudiante("Castor", 3, 9.7);
         Estudiante e5 = new Estudiante("Alejandro", 5, 8.2);
-        Estudiante e6 = new Estudiante("Alexa", 5, 9.8);
+        Estudiante e6 = new Estudiante("Alexa", 5, 10);
         Estudiante e7 = new Estudiante("Jesse", 2, 9.5);
         Estudiante e8 = new Estudiante("Yamila", 2, 9.0);
-        Estudiante e9 = new Estudiante("Alan", 1, 7.9);
-        Estudiante e10 = new Estudiante("Diego", 1, 8.0);
+        Estudiante e9 = new Estudiante("Alan", 1, 10);
+        Estudiante e10 = new Estudiante("Diego", 1, 10);
 
         // Zona de inicialización de listas
         List<String> listaDiez = new ArrayList<>();
