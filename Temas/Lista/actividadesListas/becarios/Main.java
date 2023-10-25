a841aa532ef10458d30e0673a7b4a39475c5c9c8
Carlos <20223tn066@utez.edu.mx>
2023-10-24 20:52:28 -0600

    Actividad becarios terminada

--- Temas/Lista/actividadesListas/becarios/Main.java
+++ Temas/Lista/actividadesListas/becarios/Main.java
@@ -25,8 +25,40 @@ public class Main {
         Estudiante e10 = new Estudiante("Diego", 1, 10);
 
         // Zona de inicialización de listas
-        List<String> listaDiez = new ArrayList<>();
-        List<String> listaNueve = new ArrayList<>();
+        List<Estudiante> estudiantes = new ArrayList<>();
+        List<Estudiante> listaDiez = new ArrayList<>();
+        List<Estudiante> listaNueve = new ArrayList<>();
 
+        // Zona de llenado de lista de estudiantes
+        estudiantes.add(e1);
+        estudiantes.add(e2);
+        estudiantes.add(e3);
+        estudiantes.add(e4);
+        estudiantes.add(e5);
+        estudiantes.add(e6);
+        estudiantes.add(e7);
+        estudiantes.add(e8);
+        estudiantes.add(e9);
+        estudiantes.add(e10);
+
+        // Zona de filtro de estudiantes
+        for (Estudiante estudiante : estudiantes) {
+            if (estudiante.getPromedio() >= 9.0 && estudiante.getPromedio() < 10.0) {
+                listaNueve.add(estudiante);
+            } else if (estudiante.getPromedio() == 10.0) {
+                listaDiez.add(estudiante);
+            }
+        }
+
+        // Zona de mostrado de listas
+        System.out.println("Alumnos con promedio de 10: ");
+        for (Estudiante estudiante : listaDiez) {
+            System.out.println("- " + estudiante.getNombre() + " " + estudiante.getPromedio());
+        }
+
+        System.out.println("Alumnos con promedio de 9: ");
+        for (Estudiante estudiante : listaNueve) {
+            System.out.println("- " + estudiante.getNombre() + " " + estudiante.getPromedio());
+        }
     }
 }
