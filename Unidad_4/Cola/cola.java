893d7054b0254a4013a4fd37fe3cf0b6f5b8cfff
Carlos <20223tn066@utez.edu.mx>
2023-11-09 15:27:02 -0600

    Cola

---
+++ Unidad_4/Cola/cola.java
@@ -0,0 +1,59 @@
+package Cola;
+
+public class cola<T> {
+    private static final int MAXTAM = 3;
+    private int inicio, fin;
+    private T[] lista;
+
+    public cola() {
+        inicio = 0;
+        fin = -1;
+        lista = (T[]) new Object[MAXTAM];
+    }
+
+    public boolean colaVacia() {
+        return inicio > fin;
+    }
+
+    public boolean colaLlena() {
+        return fin == MAXTAM;
+    }
+
+    public void insertar(T dato) throws Exception {
+        if (!colaLlena()) {
+            lista[++fin] = dato;
+        } else {
+            throw new Exception("Sin espacio en el arreglo");
+        }
+    }
+
+    public T quitar() throws Exception {
+        if (!colaVacia()) {
+            T aux = lista[inicio];
+            lista[inicio++] = null;
+            return lista[inicio++];
+        } else {
+            throw new Exception("Sin elementos en el arrelgo");
+        }
+    }
+
+    public void reiniciarCola() {
+        inicio = 0;
+        fin = -1;
+        lista = (T[]) new Object[MAXTAM];
+    }
+
+    public void mostrar() {
+        for (T t : lista) {
+            System.out.println(t);
+        }
+    }
+
+    public T mirarUltimo() throws Exception {
+        if (!colaVacia()) {
+            return lista[inicio];
+        } else {
+            throw new Exception("Sin elementos en el arrelgo");
+        }
+    }
+}
