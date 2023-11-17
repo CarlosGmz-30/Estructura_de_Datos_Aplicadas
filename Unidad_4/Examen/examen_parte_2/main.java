08cc25c504eacb5266f9bed62b0b8a86540245b7
Carlos <20223tn066@utez.edu.mx>
2023-11-17 11:36:16 -0600

    Cambios

---
+++ Unidad_4/Examen/examen_parte_2/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+        cola<String> cola = new cola<>();
+        try {
+            cola.insertar("Charlie");
+            cola.insertar("Keyla");
+            cola.insertar("Daniela");
+
+            cola.mostrar();
+            cola.quitar();
+            cola.mostrar();
+
+            System.out.println(cola.mirarUltimo());
+        } catch (Exception e) {
+            System.out.println("Excepcion: " + e);
+        }
+
+    }
+}
